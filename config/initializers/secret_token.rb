6e3d53e40b2cbadbd396e36076ec0c69d5b7a2ea
Dan Garland <dan@dangarland.co.uk>
2013-11-17 23:06:16 +0000

    Adding empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wegotsingles::Application.config.secret_key_base = '79b5cef43cbad07f8dcdfdd2eb88e0cb2e1276883383bbcc2268b64587291f57f44522719622826a39a52477ec9091fe6bec877f1fbabbe51edb1d06093ade60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wegotsingles::Application.config.session_store :cookie_store, key: '_wegotsingles_session'
