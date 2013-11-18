ebba20b3ca34b24cb46d68803d08976e616b3957
Dan Garland <dan@dangarland.co.uk>
2013-11-18 00:18:40 +0000

    Getting rspec going, testing homepage

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe PagesController do
+  context "GET to homepage" do
+    before do
+      get :home
+    end
 
+    it "should work" do
+      expect(response).to be_success
+    end
+  end
 end
