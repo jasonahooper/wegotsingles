7a0ceef00de817d9f5c624202e061ebd03a3fbe1
Joseph Dickinson <Joe@josephs-air.lan>
2013-11-19 20:04:43 +0000

    pulling in dev files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119184116) do
+ActiveRecord::Schema.define(version: 20131119192009) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "ethnicities", force: true do |t|
     t.string   "ethnicity"
@@ -30,10 +33,11 @@ ActiveRecord::Schema.define(version: 20131119184116) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "smoking_habits"
+    t.integer  "height"
     t.string   "occupation"
     t.string   "star_sign"
     t.integer  "star_sign_interest_level"
-    t.integer  "height"
   end
 
   create_table "users", force: true do |t|
