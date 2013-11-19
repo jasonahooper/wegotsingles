b4ffff5c4127f0284940d0d5b66274196fa8739d
Joseph Dickinson <Joe@josephs-air.lan>
2013-11-19 10:53:14 +0000

    Added date of birth and username fields to the registration page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118162200) do
+ActiveRecord::Schema.define(version: 20131119102128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20131118162200) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
+    t.string   "username"
+    t.date     "date_of_birth"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
