35cc0a74c5ffe815d3c0d2f05f2fdd53fa8deea5
Jason Hooper <jason.hooper@talk21.com>
2013-11-19 13:18:57 +0000

    refactored test to not create

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,19 +1,22 @@
 require 'spec_helper'
 
 describe ProfilesController do
-  context 'Create ethnicity on Profile' do
+  before do
+    @profile = Profile.make!
+    @ethnicity1 = Ethnicity.make!(:ethnicity => "White")
+    @ethnicity2 = Ethnicity.make!(:ethnicity => "Spanish")
+  end
+
+  context 'Add ethnicity on Profile' do
     before do
-      @profile = Profile.new
-      ethnicity1 = Ethnicity.make!(:ethnicity => "White")
-      ethnicity2 = Ethnicity.make!(:ethnicity => "African")
       @valid_params = {
           :profile =>
-          { :ethnicity_ids => [ ethnicity1.id, ethnicity2.id ]
+          { :ethnicity_ids => [ @ethnicity1.id, @ethnicity2.id ]
           }
         }
-      post :create, @valid_params
+      patch :update, @valid_params.merge(:id => @profile.id)
     end
-    it 'should store an ethnicity' do
+    it 'should store ethnicities' do
       Profile.count.should eq(1)
       ProfileEthnicity.count.should eq(2)
     end
@@ -21,18 +24,15 @@ describe ProfilesController do
 
   context 'Change ethnicity on Profile' do
     before do
-      @profile = Profile.make!
-      ethnicity1 = Ethnicity.make!(:ethnicity => "White")
-      ethnicity2 = Ethnicity.make!(:ethnicity => "Spanish")
-      @profile.ethnicities << ethnicity1
+      @profile.ethnicities << @ethnicity1
       @profile.save!
       @valid_params =
         { :profile =>
-          { :ethnicity_ids => [ ethnicity2.id ] }
+          { :ethnicity_ids => [ @ethnicity2.id ] }
         }
       patch :update, @valid_params.merge(:id => @profile.id)
     end
-    it 'should store an ethnicity' do
+    it 'should remove and add an ethnicity' do
       ProfileEthnicity.count.should eq(1)
       ProfileEthnicity.first.ethnicity_id.should
         eq(Ethnicity.find_by_ethnicity("Spanish").id)
