91b1ab7a68ef6b4038b2c211bc0252ac5f75dafb
Dan Garland <dan@dangarland.co.uk>
2013-11-21 18:43:26 +0000

    Using 320x320 for main image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [400, 400]
+  process :resize_to_fill => [320, 320]
   #
   # def scale(width, height)
   #   # do something
